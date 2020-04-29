1ca0bba48911a09c6f913ca60b15d62f67f98651
dan <Daniel Perez>
2020-04-28 19:24:28 -0500

    sdds

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(101):
+    for i in range(1, 101):
         if not(i % 3) and not(i % 5):
             print("FizzBuzz", end=" ")
         elif not(i % 3):
