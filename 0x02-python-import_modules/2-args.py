793a05feb982019f7ae8577616484f89b7d87e5c
dan <Daniel Perez>
2020-04-30 18:17:56 -0500

    sdadas

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
         print("{} argument.".format(l - 1))
     else:
         print("{} arguments:".format(l - 1))
-        for i in range(1, l):
-            print("{}: {}".format(i, arg[i]))
+    for i in range(1, l):
+        print("{}: {}".format(i, arg[i]))
