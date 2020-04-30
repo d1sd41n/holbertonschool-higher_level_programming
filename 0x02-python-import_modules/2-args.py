1445c1aeeff900d11b297121da90d10f225757a9
dan <Daniel Perez>
2020-04-30 18:10:11 -0500

    2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    arg = sys.argv
+    l = len(arg)
+    if l == 1:
+        print("{} arguments.".format(l - 1))
+    elif l == 2:
+        print("{} argument.".format(l - 1))
+    else:
+        print("{} arguments.".format(l - 1))
+        for i in range(1, l):
+            print("{}: {}".format(i, arg[i]))
+
+
