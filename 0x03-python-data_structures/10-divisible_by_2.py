c20bb6a781484b4f61ef03414361c0a36354fd34
dan <Daniel Perez>
2020-05-05 20:50:07 -0500

    10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+    n = []
+    for i in my_list:
+        if i % 2 == 0:
+            n.append(True)
+        else:
+            n.append(false)
+    return n
+
+        
