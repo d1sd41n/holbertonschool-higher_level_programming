2e515eb8874dd1206a5f1bf08a68c851201ee7ec
Daniel Alejandro <outnumbered@live.com>
2020-05-08 07:27:30 -0500

    9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
 
-    b_score = max(a_dictionary.values(), default=None)
-    for k, v in a_dictionary.items():
-        if v == b_score:
-            return k
+
+def multiply_by_2(a_dictionary):
+    n = {}
+    for i in a_dictionary.items():
+        n[i[0]] = a_dictionary[i[0]] * 2
+    return n
