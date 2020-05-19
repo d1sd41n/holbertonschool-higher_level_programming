82ae07177c74b44a9bf871b91ab98063ed695cab
dan <Daniel Perez>
2020-05-19 17:01:07 -0500

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""square class module
+"""
+
+
+class Square:
+    """class square
+    """
+    def __init__(self, size):
+        """__init__
+
+        Arguments:
+            size {int} -- is the size
+        """
+        self.__size = size
