5250db4e6892f74b1ec7a1dc3fdc3f862ffedd3f
dan <Daniel Perez>
2020-05-19 17:44:18 -0500

    3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""square class module
+"""
+
+
+class Square:
+    """class square
+    """
+    def __init__(self, size=0):
+        """__init__
+
+        Arguments:
+            size {int} -- is the size
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """[area]
+
+        Returns:
+            int -- square of size
+        """
+        return self.__size ** 2
