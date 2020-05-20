e5f41e44961acc91c33e4041546ccc6de74ee377
dan <Daniel Perez>
2020-05-19 19:03:50 -0500

    5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""square class module
+"""
+
+
+class Square:
+    """class square
+    """
+    def __init__(self, size=0):
+        """__init__
+
+        Arguments:
+            size {int} -- is the size
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """[area]
+
+        Returns:
+            int -- square of size
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """[size]
+
+        Returns:
+            int -- size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """[summary]
+
+        Arguments:
+            value {[value]} -- [description]
+
+        Raises:
+            TypeError: [size must be an integer]
+            TypeError: [size must be >= 0]
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """[prints square]
+        """
+        s = self.__size
+        if s == 0:
+            print()
+        else:
+            for i in range(s):
+                print("#" * s)
