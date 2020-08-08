6f850fc260f9a39feb4ed94af03b5e04a02aec9b
Daniel Alejandro <outnumbered@live.com>
2020-08-08 13:24:18 -0500

    fixing

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -4,6 +4,7 @@
 import sys
 import MySQLdb
 
+
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
