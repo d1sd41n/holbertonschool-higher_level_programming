927388d6c03667a4c5984032b261d0764b055f03
Daniel Alejandro <outnumbered@live.com>
2020-08-24 22:07:09 -0500

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """get an status
 """
-from urllib.request import Request, urlopen
-
+import urllib.request
 
 if __name__ == "__main__":
-    with urlopen('https://intranet.hbtn.io/status') as re:
-        r = re.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as re:
+        html = re.read()
         print('Body response:')
-        print('\t- type: {}'.format(r))
-        print('\t- content: {}'.format(r))
-        print('\t- utf8 content: {}'.format(r.decode('utf-8')))
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
