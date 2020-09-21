169ebf71f967dd524aece0cfbdc52d5c130c7e94
dis dain <ddd666.dt@gmail.com>
2020-09-21 13:40:59 -0500

    4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const tmph = this.height;
+    const tmpw = this.width;
+    this.height = tmpw;
+    this.width = tmph;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
